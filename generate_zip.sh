89216ef2249476779dc072735d3fbc26fe88f7a4
Koenkk <koenkanters94@gmail.com>
2017-01-18 17:53:09 +0100

    Add OnePlus 3 firmware (from OnePlus3Oxygen_16_OTA_039_all_1701140133_89fcc9baba94e4c.zip)

--- generate_zip.sh
+++ generate_zip.sh
@@ -5,3 +5,7 @@ rm -rf *.zip
 
 # Generate ZIP for OnePlus 3T
 cd oneplus3t && zip -r ../OnePlus3T_4.0.2_firmware.zip *
+cd ..
+
+# Generate ZIP for OnePlus 3
+cd oneplus3 && zip -r ../OnePlus3_4.0.2_firmware.zip *

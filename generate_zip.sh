8fa4bfcebf3b20bb82b86b0fa9ec59080facc954
Unknown <koenkanters94@gmail.com>
2017-02-16 19:00:49 +0100

    Import op3 4.0.3 (OnePlus3Oxygen_16_OTA_041_all_1702081756_0f1e56a75f)

--- generate_zip.sh
+++ generate_zip.sh
@@ -8,4 +8,4 @@ cd oneplus3t && zip -r ../OnePlus3T_4.0.2_firmware.zip *
 cd ..
 
 # Generate ZIP for OnePlus 3
-cd oneplus3 && zip -r ../OnePlus3_4.0.2_firmware.zip *
+cd oneplus3 && zip -r ../OnePlus3_4.0.3_firmware.zip *

d54568da028aeb5ced5ea2ee2c008b3fb76f9ce4
Koenkk <koenkanters94@gmail.com>
2017-07-19 16:20:47 +0200

    op3(t): update zip file names.

--- generate_zip.sh
+++ generate_zip.sh
@@ -4,11 +4,11 @@
 rm -rf *.zip
 
 # Generate ZIP for OnePlus 3T
-cd oneplus3t && zip -r ../OnePlus3T_Open_Beta_3_firmware.zip *
+cd oneplus3t && zip -r ../OnePlus3T_Open_Beta_11_firmware.zip *
 cd ..
 
 # Generate ZIP for OnePlus 3
-cd oneplus3 && zip -r ../OnePlus3_Open_Beta_12_firmware.zip *
+cd oneplus3 && zip -r ../OnePlus3_Open_Beta_20_firmware.zip *
 cd ..
 
 # Generate ZIP for OnePlus 5
